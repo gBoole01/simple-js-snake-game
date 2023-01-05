5d29d8a3f682b0419d28347dbe7554e65e1b7bad
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-05 21:59:49 +0100

    feat: add food element

--- snake.js
+++ snake.js
@@ -2,8 +2,11 @@ import { getInputDirection } from "./input"
 
 export const SNAKE_SPEED = 2
 const snakeBody = [{ x: 11, y: 11 }]
+let newSegments = 0;
 
 export function update() {
+    addSegments()
+
     const inputDirection = getInputDirection()
     for (let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = { ...snakeBody[i] }
@@ -22,4 +25,26 @@ export function draw(gameBoard) {
         gameBoard.appendChild(snakeElement)
     })
     console.log("Drawn")
+}
+
+export function expandSnake(amount) {
+    newSegments += amount
+}
+
+export function onSnake(position) {
+    return snakeBody.some(segment => {
+        return equalPosition(segment, position)
+    })
+}
+
+function equalPosition(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push({ ...snakeBody[snakeBody.length - 1] })
+    }
+
+    newSegments = 0
 }
