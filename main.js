3e5d0f815a26738494244388c1abcd5313066a77
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-05 22:07:35 +0100

    feat add game over scenario

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake"
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from "./snake"
 import { update as updateFood, draw as drawFood } from "./food"
+import { outsideGrid } from "./grid"
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById("game-board")
 
 function main(currentTime) {
+  if (gameOver) {
+    return alert('you lose')
+  }
+
   window.requestAnimationFrame(main)
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
@@ -19,10 +25,15 @@ window.requestAnimationFrame(main)
 function update() {
   updateSnake()
   updateFood()
+  checkDeath()
 }
 
 function draw() {
   gameBoard.innerHTML = ""
   drawSnake(gameBoard)
   drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
